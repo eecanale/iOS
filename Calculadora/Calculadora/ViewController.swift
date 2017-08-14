f3de9ddef614923afa95de1cd7f48101ff624589
Eleazar Canale <eecanale@yahoo.com>
2017-08-14 12:54:33 -0700

    Ejercicios iniciales

---
+++ Calculadora/Calculadora/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Calculadora
+//
+//  Created by Eleazar Canale on 8/14/17.
+//  Copyright © 2017 Caliente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var var1: UITextField!
+    @IBOutlet weak var var2: UITextField!
+    @IBOutlet weak var result: UITextField!
+    
+    @IBAction func Suma(_ sender: UIButton) {
+        result.text = String(Int(var1.text!)! + Int(var2.text!)!)
+    }
+    
+    @IBAction func Resta(_ sender: UIButton) {
+        result.text = String(Int(var1.text!)! - Int(var2.text!)!)
+    }
+    
+    @IBAction func Multi(_ sender: UIButton) {
+        result.text = String(Int(var1.text!)! * Int(var2.text!)!)
+    }
+    
+    @IBAction func Div(_ sender: UIButton) {
+        result.text = String(Int(var1.text!)! / Int(var2.text!)!)
+    }
+    
+}
+
---
+++ Ejemplo1.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground" //Implicito
+var str2:String = "Hola mundo" //Explicito
+
+var optional:String? = nil
+
+let const = "SI"
+
+var var1 = 5
+var var2 = 1
+
+var var3 = var1 - var2
