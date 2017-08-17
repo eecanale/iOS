31d1bcc8e2255572440e5807faaae894f657c908
Eleazar Canale <eecanale@yahoo.com>
2017-08-16 17:22:27 -0700

    Clase 3

--- MapView/MapView/ViewController.swift
+++ MapView/MapView/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let location = CLLocationCoordinate2D(latitude: 32.505562, longitude: -117.0000479)
+        let span = MKCoordinateSpanMake(0.5, 0.5)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Estadio"
+        annotation.subtitle = "Caliente"
+        mapView.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
