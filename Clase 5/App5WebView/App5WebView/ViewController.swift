ce6855a8e3082ee947cbf8a504fbc77b247c88fb
Eleazar Canale <eecanale@yahoo.com>
2017-08-18 13:08:11 -0700

    Clase 5

--- Clase 5/App5WebView/App5WebView/ViewController.swift
+++ Clase 5/App5WebView/App5WebView/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var textField: UITextField!
+    
+    var palabra:String?
+    /*URL = https://es.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=sega */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let google = URL(string: "https://www.google.com")
+        
+        webView.loadRequest(URLRequest(url: google!))
+        
+        // request.httpMethod = "POST"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buscar(_ sender: Any) {
+        palabra = textField.text!
+        let urlCompleto = "https://es.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=\(palabra!)"
+        let objetoUrl = URL(string:urlCompleto)
+        
+        //Peticion realizar tarea
+        let tarea = URLSession.shared.dataTask(with: objetoUrl!) {(datos, respuesta, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                do {
+                    // Se obtiene objeto JSON dentro de la Ap
+                    let json = try JSONSerialization.jsonObject(with: datos!, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String:Any]
+                    print(json)
+                    
+                    let querySubjson = json["query"] as! [String:Any]
+                    print(querySubjson)
+                    
+                    let pagesSubjson = querySubjson["pages"] as! [String:Any]
+                    print(pagesSubjson)
+                    
+                    let idSubjson = pagesSubjson["23597"] as! [String:Any]
+                    print(idSubjson)
+                    
+                    let extractStringHtml = idSubjson["extract"] as! String
+                    print(extractStringHtml)
+                    
+                    DispatchQueue.main.sync(execute: {
+                        self.webView.loadHTMLString(extractStringHtml, baseURL: nil)
+                    })
+                    
+                } catch {
+                    print("El procesamiento del JSON tuvo un error")
+                }
+            }
+        }
+        tarea.resume()
+    }
 
 }
 
